3b9e5ae7a28ac339c13b4b30897294b06edc480e
Minah Kim <kimma1205@gmail.com>
2025-08-13 02:27:56 +0900

    우측 사이드바 정적 HTML 추가

--- src/components/RightSidebar.tsx
+++ src/components/RightSidebar.tsx
@@ -1,4 +1,75 @@
 export default function RightSidebar() {
-    return <div style={{ padding: 12 }}>Right-Sidebar</div>;
-  }
-  
+  return (
+    <div style={{ padding: '16px' }}>
+      <aside>
+        <div>
+          {/* 앨범자켓 (비디오 없을 시 비디오 대신 표시) */}
+          <div>
+
+          </div>
+
+          {/* 비디오 */}
+          <div>
+
+          </div>
+
+          {/* 노래제목, 아티스트명 */}
+          <div>
+
+          </div>
+
+          {/* Related music videos */}
+          <div>
+
+          </div>
+
+          {/* About the artist */}
+          <div>
+
+          </div>
+
+          {/* Credits */}
+          <div style={{ padding: '16px', gap: '12px', display: 'grid', border: '1px solid #242430' }}>
+            <div>Credits</div>
+            <div style={{ width: '356px', height: '44px' }}>
+              <div>
+                <a>
+                  <span>U-KNOW</span>
+                </a>
+              </div>
+              <div>
+                <span>Main Artist</span>
+              </div>
+            </div>
+            <div style={{ width: '356px', height: '44px' }}>
+              <div>
+                <a>
+                  <span>Young-Jin Yoo
+                  </span>
+                </a>
+              </div>
+              <div>
+                <span>Composer, Arranger, Lyricist</span>
+              </div>
+            </div>
+            <div style={{ width: '356px', height: '44px' }}>
+              <div>
+                <a>
+                  <span>Thomas Troelsen</span>
+                </a>
+              </div>
+              <div>
+                <span>Composer, Arranger</span>
+              </div>
+            </div>
+          </div>
+
+          {/* Next in queue */}
+          <div>
+
+          </div>
+        </div>
+      </aside>
+    </div>
+  );
+}
