b5b4f1cdee192c28fb7cd09fbb177e17328dc9ad
Minah Kim <kimma1205@gmail.com>
2025-08-12 22:24:59 +0900

    불필요한 import 제거

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,3 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
