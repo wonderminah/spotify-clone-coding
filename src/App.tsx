c648d2d1d95c983604c5129d682b24f178ed073c
Minah Kim <kimma1205@gmail.com>
2025-08-12 22:24:01 +0900

    React Router 인스톨 하고 라우팅 처리 추가

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,35 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Album from "./pages/Album";
+import Artist from "./pages/Artist";
+import Home from "./pages/Home";
+import Playlist from "./pages/Playlist";
+import Search from "./pages/Search";
+import Section from "./pages/Section";
+
 function App() {
   return (
-    <></>
+    <Router>
+      <nav className="bg-gray-900 text-white p-4 flex gap-4">
+        <Link to="/album" className="hover:text-green-500">앨범</Link>
+        <Link to="/artist" className="hover:text-green-500">아티스트</Link>
+        <Link to="/" className="hover:text-green-500">홈</Link>
+        <Link to="/playlist" className="hover:text-green-500">플레이리스트</Link>
+        <Link to="/search" className="hover:text-green-500">검색</Link>
+        <Link to="/section" className="hover:text-green-500">섹션</Link>
+      </nav>
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/album" element={<Album />} />
+        <Route path="/artist" element={<Artist />} />
+        <Route path="/playlist" element={<Playlist />} />
+        <Route path="/search" element={<Search />} />
+        <Route path="/section" element={<Section />} />
+      </Routes>
+    </Router>
   )
 }
 
---
+++ src/pages/Section.tsx
@@ -0,0 +1,8 @@
+export default function Section() {
+    return (
+        <div className="p-6">
+            <h1 className="text-2xl font-bold">홈 페이지</h1>
+            <p>여기는 Spotify 클론의 Section 화면입니다.</p>
+        </div>
+    );
+}
