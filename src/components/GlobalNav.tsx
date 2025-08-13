5185d8cb529c4938e68dbd54548ed1d9b6049912
Minah Kim <kimma1205@gmail.com>
2025-08-14 03:26:22 +0900

    인덴트 수정

--- src/components/GlobalNav.tsx
+++ src/components/GlobalNav.tsx
@@ -3,12 +3,12 @@ import { Link } from "react-router-dom";
 export default function GlobalNav() {
   return (
     <nav className="text-white p-3 flex gap-4">
-        <Link to="/album" className="hover:text-green-500">Album</Link>
-        <Link to="/artist" className="hover:text-green-500">Artist</Link>
-        <Link to="/" className="hover:text-green-500">Home</Link>
-        <Link to="/playlist" className="hover:text-green-500">Playlist</Link>
-        <Link to="/search" className="hover:text-green-500">Search</Link>
-        <Link to="/section" className="hover:text-green-500">Section</Link>
-      </nav>
+      <Link to="/album" className="hover:text-green-500">Album</Link>
+      <Link to="/artist" className="hover:text-green-500">Artist</Link>
+      <Link to="/" className="hover:text-green-500">Home</Link>
+      <Link to="/playlist" className="hover:text-green-500">Playlist</Link>
+      <Link to="/search" className="hover:text-green-500">Search</Link>
+      <Link to="/section" className="hover:text-green-500">Section</Link>
+    </nav>
   );
 }
