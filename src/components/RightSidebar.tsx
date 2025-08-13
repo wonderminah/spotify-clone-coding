bd2de3a5c77746351ea389d5ebaa47008ba193ac
Minah Kim <kimma1205@gmail.com>
2025-08-13 20:43:23 +0900

    RightSidebar 에 내용 추가

--- src/components/RightSidebar.tsx
+++ src/components/RightSidebar.tsx
@@ -1,31 +1,73 @@
+// RightSidebar 에만 스크롤 주려면 어떻게?
 export default function RightSidebar() {
   return (
     <div style={{ padding: '16px' }}>
       <aside>
-        <div>
-          {/* 앨범자켓 (비디오 없을 시 비디오 대신 표시) */}
-          <div>
-
+        <div style={{ gap: '16px', display: 'grid' }}>
+          {/* 오디오 선택 시: 영상 */}
+          {/* 비디오 선택 시: 뮤직비디오 */}
+          <div style={{ padding: '16px', border: '1px solid #242430' }}>
+            뮤직비디오
           </div>
 
-          {/* 비디오 */}
-          <div>
-
+          {/* 오디오/비디오 스위치 버튼 */}
+          <div style={{ padding: '16px', border: '1px solid #242430' }}>
+            <button>Switch to audio</button>
           </div>
 
           {/* 노래제목, 아티스트명 */}
-          <div>
-
+          <div style={{ padding: '16px', border: '1px solid #242430' }}>
+            노래제목, 아티스트명
+            <div>
+              <a>
+                <span>Follow</span>
+              </a>
+            </div>
+            <div>
+              <a>
+                <span>U-KNOW</span>
+              </a>
+            </div>
           </div>
 
           {/* Related music videos */}
-          <div>
-
+          <div style={{ padding: '16px', border: '1px solid #242430' }}>
+            <div>Related music videos</div>
+            <div style={{ overflowX: 'auto' }}>
+              <div style={{ width: '210px', height: '177px', border: '1px solid #242430' }}>
+                <div>비디오 썸네일</div>
+                <div>WICKED</div>
+                <div>ALLDAY PROJECT</div>
+              </div>
+              <div style={{ width: '210px', height: '177px', border: '1px solid #242430' }}>
+                <div>비디오 썸네일</div>
+                <div>WICKED</div>
+                <div>ALLDAY PROJECT</div>
+              </div>
+              <div style={{ width: '210px', height: '177px', border: '1px solid #242430' }}>
+                <div>비디오 썸네일</div>
+                <div>WICKED</div>
+                <div>ALLDAY PROJECT</div>
+              </div>
+              <div style={{ width: '210px', height: '177px', border: '1px solid #242430' }}>
+                <div>비디오 썸네일</div>
+                <div>WICKED</div>
+                <div>ALLDAY PROJECT</div>
+              </div>
+            </div>
           </div>
 
           {/* About the artist */}
-          <div>
-
+          <div style={{ padding: '16px', border: '1px solid #242430' }}>
+            <div>About the artist</div>
+            <div style={{ width: '356px', height: '242px' }}>
+              사진
+            </div>
+            <div style={{ width: '356px', height: '142px' }}>
+              <div>U-KNOW</div>
+              <div>68,089 monthly listeners</div>
+              <div>U-KNOW made his debut in 2003 as a member of TVXQ!, "The Rising Gods of the East", well-known as the "Kings of K-pop" for their immense success and...</div>
+            </div>
           </div>
 
           {/* Credits */}
