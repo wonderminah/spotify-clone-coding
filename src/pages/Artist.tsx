49aec57ebae2b36d6cc770b48cab648f28e61ea3
Minah Kim <kimma1205@gmail.com>
2025-08-13 20:42:47 +0900

    그냥 인덴트 수정

--- src/pages/Artist.tsx
+++ src/pages/Artist.tsx
@@ -1,8 +1,8 @@
 export default function Artist() {
     return (
-      <div className="p-6">
-        <h1 className="text-2xl font-bold">홈 페이지</h1>
-        <p>여기는 Spotify 클론의 Artist 화면입니다.</p>
-      </div>
+        <div className="p-6">
+            <h1 className="text-2xl font-bold">홈 페이지</h1>
+            <p>여기는 Spotify 클론의 Artist 화면입니다.</p>
+        </div>
     );
-  }
+}
