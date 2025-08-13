eef373945433891cf76e944fcc2e605d360cb735
Minah Kim <kimma1205@gmail.com>
2025-08-14 00:38:56 +0900

    홈 화면 세로 섹션 나누기

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,45 @@
 export default function Home() {
     return (
-        <div className="p-6">
-            <h1 className="text-2xl font-bold">홈 페이지</h1>
-            <p>여기는 Spotify 클론의 Home 화면입니다.</p>
+        <div>
+            <div className="p-6">
+                <h1 className="text-2xl font-bold">홈 페이지</h1>
+                <p>여기는 Spotify 클론의 Home 화면입니다.</p>
+            </div>
+
+            {/* All, Music, Podcasts */}
+            <div></div>
+
+            {/* 타일 8개 */}
+            <div></div>
+
+            {/* Made For Mina Kim */}
+            <div></div>
+
+            {/* Jump back in */}
+            <div></div>
+
+            {/* Recently played */}
+            <div></div>
+
+            {/* New releases for you */}
+            <div></div>
+
+            {/* Your top mixes */}
+            <div></div>
+
+            {/* Recommended for today */}
+            <div></div>
+
+            {/* Your favorite artists */}
+            <div></div>
+
+            {/* Based on your recent listening */}
+            <div></div>
+
+            {/* Albums featuring songs you like */}
+            <div></div>
+            
+            {/* K-Indie,Rock & Various Genres */}
         </div>
     );
 }
