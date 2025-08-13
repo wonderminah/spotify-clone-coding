bd47818a5bb0f8bd9bef9c57a5631dec9e27e095
Minah Kim <kimma1205@gmail.com>
2025-08-14 02:22:05 +0900

    LeftSidebar 큰 틀 구성

--- src/components/LeftSidebar.tsx
+++ src/components/LeftSidebar.tsx
@@ -1,4 +1,45 @@
 export default function LeftSidebar() {
-    return <div style={{ padding: 12 }}>Left-Sidebar</div>;
-  }
-  
+  return (
+    <div style={{ height: 'calc(100vh - 140px)', overflowY: 'auto', padding: '16px' }}>
+      {/* Header */}
+      <div>
+        {/* Your Library */}
+        <div style={{ height: '56px' }}></div>
+
+        {/* Buttons */}
+        <div style={{ height: '50px' }}></div>
+      </div>
+
+      {/* List */}
+      <div>
+        {/* Search / Filter */}
+        <div style={{ height: '34px' }}></div>
+
+        {/* Result */}
+        <div>
+          <div style={{ height: '64px' }}></div>
+          <div style={{ height: '64px' }}></div>
+          <div style={{ height: '64px' }}></div>
+          <div style={{ height: '64px' }}></div>
+          <div style={{ height: '64px' }}></div>
+          <div style={{ height: '64px' }}></div>
+          <div style={{ height: '64px' }}></div>
+          <div style={{ height: '64px' }}></div>
+          <div style={{ height: '64px' }}></div>
+          <div style={{ height: '64px' }}></div>
+          <div style={{ height: '64px' }}></div>
+          <div style={{ height: '64px' }}></div>
+          <div style={{ height: '64px' }}></div>
+          <div style={{ height: '64px' }}></div>
+          <div style={{ height: '64px' }}></div>
+          <div style={{ height: '64px' }}></div>
+          <div style={{ height: '64px' }}></div>
+          <div style={{ height: '64px' }}></div>
+          <div style={{ height: '64px' }}></div>
+          <div style={{ height: '64px' }}></div>
+        </div>
+      </div>
+
+    </div>
+  );
+}
