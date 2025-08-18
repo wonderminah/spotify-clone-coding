46697b7885914b66872c0a54b19376b6bc472f3f
Minah Kim <kimma1205@gmail.com>
2025-08-19 01:05:13 +0900

    LeftSidebar 결과값 UI 수정

--- src/components/LeftSidebar.tsx
+++ src/components/LeftSidebar.tsx
@@ -1,45 +1,283 @@
 export default function LeftSidebar() {
   return (
-    <div style={{ height: 'calc(100vh - 140px)', overflowY: 'auto', padding: '16px' }}>
-      {/* Header */}
-      <div>
-        {/* Your Library */}
-        <div style={{ height: '56px' }}></div>
+    <div style={{ borderRadius: '8px', height: 'calc(100vh - 140px)', overflowY: 'auto', padding: '8px' }}>
+      {/* 상단 헤더 */}
+      <div style={{ margin: '8px' }}>
+        <header style={{ height: '56px' }}>
+          <div style={{ display: 'flex' }}>
+            {/* Your Library 타이틀 */}
+            <div>
+              <button>Your Library</button>
+            </div>
+            {/* Create 버튼 / 확대 버튼 */}
+            <div style={{ marginLeft: 'auto' }}>
+              <button>Create</button>
+              <button>확대</button>
+            </div>
+          </div>
+        </header>
 
-        {/* Buttons */}
-        <div style={{ height: '50px' }}></div>
+        {/* 전환 버튼 */}
+        <div style={{ height: '40px', display: 'flex', columnGap: '8px', alignItems: 'center', overflowX: 'auto', whiteSpace: 'nowrap' }}>
+          <div style={{ border: '1px solid #818181', borderRadius: '9999px', padding: '4px 12px' }}>Playlists</div>
+          <div style={{ border: '1px solid #818181', borderRadius: '9999px', padding: '4px 12px' }}>Artists</div>
+          <div style={{ border: '1px solid #818181', borderRadius: '9999px', padding: '4px 12px' }}>Albums</div>
+          <div style={{ border: '1px solid #818181', borderRadius: '9999px', padding: '4px 12px' }}>Albums</div>
+          <div style={{ border: '1px solid #818181', borderRadius: '9999px', padding: '4px 12px' }}>Podcast & Shows</div>
+        </div>
       </div>
 
-      {/* List */}
+      {/* 하단 리스트 */}
       <div>
         {/* Search / Filter */}
         <div style={{ height: '34px' }}></div>
 
-        {/* Result */}
+
+
+        {/* 리스트 필터링 결과값 */}
         <div>
-          <div style={{ height: '64px' }}></div>
-          <div style={{ height: '64px' }}></div>
-          <div style={{ height: '64px' }}></div>
-          <div style={{ height: '64px' }}></div>
-          <div style={{ height: '64px' }}></div>
-          <div style={{ height: '64px' }}></div>
-          <div style={{ height: '64px' }}></div>
-          <div style={{ height: '64px' }}></div>
-          <div style={{ height: '64px' }}></div>
-          <div style={{ height: '64px' }}></div>
-          <div style={{ height: '64px' }}></div>
-          <div style={{ height: '64px' }}></div>
-          <div style={{ height: '64px' }}></div>
-          <div style={{ height: '64px' }}></div>
-          <div style={{ height: '64px' }}></div>
-          <div style={{ height: '64px' }}></div>
-          <div style={{ height: '64px' }}></div>
-          <div style={{ height: '64px' }}></div>
-          <div style={{ height: '64px' }}></div>
-          <div style={{ height: '64px' }}></div>
+          <div style={{ display: 'flex', columnGap: '12px', alignItems: 'center', padding: '8px' }}>
+            <div style={{ width: '55px' }}>
+              <img style={{ borderRadius: '6px' }} src="https://i.scdn.co/image/ab67616d00001e024ba287ffc98735a7c801be3e"></img>
+            </div>
+            <div>
+              <p>Liked Songs</p>
+              <div>
+                <span>핀</span>
+                <span>Playlist・631 songs</span>
+              </div>
+            </div>
+          </div>
+          <div style={{ display: 'flex', columnGap: '12px', alignItems: 'center', padding: '8px' }}>
+            <div style={{ width: '55px' }}>
+              <img style={{ borderRadius: '6px' }} src="https://i.scdn.co/image/ab67616d00001e024ba287ffc98735a7c801be3e"></img>
+            </div>
+            <div>
+              <p>Liked Songs</p>
+              <div>
+                <span>핀</span>
+                <span>Playlist・631 songs</span>
+              </div>
+            </div>
+          </div>
+          <div style={{ display: 'flex', columnGap: '12px', alignItems: 'center', padding: '8px' }}>
+            <div style={{ width: '55px' }}>
+              <img style={{ borderRadius: '6px' }} src="https://i.scdn.co/image/ab67616d00001e024ba287ffc98735a7c801be3e"></img>
+            </div>
+            <div>
+              <p>Liked Songs</p>
+              <div>
+                <span>핀</span>
+                <span>Playlist・631 songs</span>
+              </div>
+            </div>
+          </div>
+          <div style={{ display: 'flex', columnGap: '12px', alignItems: 'center', padding: '8px' }}>
+            <div style={{ width: '55px' }}>
+              <img style={{ borderRadius: '6px' }} src="https://i.scdn.co/image/ab67616d00001e024ba287ffc98735a7c801be3e"></img>
+            </div>
+            <div>
+              <p>Liked Songs</p>
+              <div>
+                <span>핀</span>
+                <span>Playlist・631 songs</span>
+              </div>
+            </div>
+          </div>
+          <div style={{ display: 'flex', columnGap: '12px', alignItems: 'center', padding: '8px' }}>
+            <div style={{ width: '55px' }}>
+              <img style={{ borderRadius: '6px' }} src="https://i.scdn.co/image/ab67616d00001e024ba287ffc98735a7c801be3e"></img>
+            </div>
+            <div>
+              <p>Liked Songs</p>
+              <div>
+                <span>핀</span>
+                <span>Playlist・631 songs</span>
+              </div>
+            </div>
+          </div>
+          <div style={{ display: 'flex', columnGap: '12px', alignItems: 'center', padding: '8px' }}>
+            <div style={{ width: '55px' }}>
+              <img style={{ borderRadius: '6px' }} src="https://i.scdn.co/image/ab67616d00001e024ba287ffc98735a7c801be3e"></img>
+            </div>
+            <div>
+              <p>Liked Songs</p>
+              <div>
+                <span>핀</span>
+                <span>Playlist・631 songs</span>
+              </div>
+            </div>
+          </div>
+          <div style={{ display: 'flex', columnGap: '12px', alignItems: 'center', padding: '8px' }}>
+            <div style={{ width: '55px' }}>
+              <img style={{ borderRadius: '6px' }} src="https://i.scdn.co/image/ab67616d00001e024ba287ffc98735a7c801be3e"></img>
+            </div>
+            <div>
+              <p>Liked Songs</p>
+              <div>
+                <span>핀</span>
+                <span>Playlist・631 songs</span>
+              </div>
+            </div>
+          </div>
+          <div style={{ display: 'flex', columnGap: '12px', alignItems: 'center', padding: '8px' }}>
+            <div style={{ width: '55px' }}>
+              <img style={{ borderRadius: '6px' }} src="https://i.scdn.co/image/ab67616d00001e024ba287ffc98735a7c801be3e"></img>
+            </div>
+            <div>
+              <p>Liked Songs</p>
+              <div>
+                <span>핀</span>
+                <span>Playlist・631 songs</span>
+              </div>
+            </div>
+          </div>
+          <div style={{ display: 'flex', columnGap: '12px', alignItems: 'center', padding: '8px' }}>
+            <div style={{ width: '55px' }}>
+              <img style={{ borderRadius: '6px' }} src="https://i.scdn.co/image/ab67616d00001e024ba287ffc98735a7c801be3e"></img>
+            </div>
+            <div>
+              <p>Liked Songs</p>
+              <div>
+                <span>핀</span>
+                <span>Playlist・631 songs</span>
+              </div>
+            </div>
+          </div>
+          <div style={{ display: 'flex', columnGap: '12px', alignItems: 'center', padding: '8px' }}>
+            <div style={{ width: '55px' }}>
+              <img style={{ borderRadius: '6px' }} src="https://i.scdn.co/image/ab67616d00001e024ba287ffc98735a7c801be3e"></img>
+            </div>
+            <div>
+              <p>Liked Songs</p>
+              <div>
+                <span>핀</span>
+                <span>Playlist・631 songs</span>
+              </div>
+            </div>
+          </div>
+          <div style={{ display: 'flex', columnGap: '12px', alignItems: 'center', padding: '8px' }}>
+            <div style={{ width: '55px' }}>
+              <img style={{ borderRadius: '6px' }} src="https://i.scdn.co/image/ab67616d00001e024ba287ffc98735a7c801be3e"></img>
+            </div>
+            <div>
+              <p>Liked Songs</p>
+              <div>
+                <span>핀</span>
+                <span>Playlist・631 songs</span>
+              </div>
+            </div>
+          </div>
+          <div style={{ display: 'flex', columnGap: '12px', alignItems: 'center', padding: '8px' }}>
+            <div style={{ width: '55px' }}>
+              <img style={{ borderRadius: '6px' }} src="https://i.scdn.co/image/ab67616d00001e024ba287ffc98735a7c801be3e"></img>
+            </div>
+            <div>
+              <p>Liked Songs</p>
+              <div>
+                <span>핀</span>
+                <span>Playlist・631 songs</span>
+              </div>
+            </div>
+          </div>
+          <div style={{ display: 'flex', columnGap: '12px', alignItems: 'center', padding: '8px' }}>
+            <div style={{ width: '55px' }}>
+              <img style={{ borderRadius: '6px' }} src="https://i.scdn.co/image/ab67616d00001e024ba287ffc98735a7c801be3e"></img>
+            </div>
+            <div>
+              <p>Liked Songs</p>
+              <div>
+                <span>핀</span>
+                <span>Playlist・631 songs</span>
+              </div>
+            </div>
+          </div>
+          <div style={{ display: 'flex', columnGap: '12px', alignItems: 'center', padding: '8px' }}>
+            <div style={{ width: '55px' }}>
+              <img style={{ borderRadius: '6px' }} src="https://i.scdn.co/image/ab67616d00001e024ba287ffc98735a7c801be3e"></img>
+            </div>
+            <div>
+              <p>Liked Songs</p>
+              <div>
+                <span>핀</span>
+                <span>Playlist・631 songs</span>
+              </div>
+            </div>
+          </div>
+          <div style={{ display: 'flex', columnGap: '12px', alignItems: 'center', padding: '8px' }}>
+            <div style={{ width: '55px' }}>
+              <img style={{ borderRadius: '6px' }} src="https://i.scdn.co/image/ab67616d00001e024ba287ffc98735a7c801be3e"></img>
+            </div>
+            <div>
+              <p>Liked Songs</p>
+              <div>
+                <span>핀</span>
+                <span>Playlist・631 songs</span>
+              </div>
+            </div>
+          </div>
+          <div style={{ display: 'flex', columnGap: '12px', alignItems: 'center', padding: '8px' }}>
+            <div style={{ width: '55px' }}>
+              <img style={{ borderRadius: '6px' }} src="https://i.scdn.co/image/ab67616d00001e024ba287ffc98735a7c801be3e"></img>
+            </div>
+            <div>
+              <p>Liked Songs</p>
+              <div>
+                <span>핀</span>
+                <span>Playlist・631 songs</span>
+              </div>
+            </div>
+          </div>
+          <div style={{ display: 'flex', columnGap: '12px', alignItems: 'center', padding: '8px' }}>
+            <div style={{ width: '55px' }}>
+              <img style={{ borderRadius: '6px' }} src="https://i.scdn.co/image/ab67616d00001e024ba287ffc98735a7c801be3e"></img>
+            </div>
+            <div>
+              <p>Liked Songs</p>
+              <div>
+                <span>핀</span>
+                <span>Playlist・631 songs</span>
+              </div>
+            </div>
+          </div>
+          <div style={{ display: 'flex', columnGap: '12px', alignItems: 'center', padding: '8px' }}>
+            <div style={{ width: '55px' }}>
+              <img style={{ borderRadius: '6px' }} src="https://i.scdn.co/image/ab67616d00001e024ba287ffc98735a7c801be3e"></img>
+            </div>
+            <div>
+              <p>Liked Songs</p>
+              <div>
+                <span>핀</span>
+                <span>Playlist・631 songs</span>
+              </div>
+            </div>
+          </div>
+          <div style={{ display: 'flex', columnGap: '12px', alignItems: 'center', padding: '8px' }}>
+            <div style={{ width: '55px' }}>
+              <img style={{ borderRadius: '6px' }} src="https://i.scdn.co/image/ab67616d00001e024ba287ffc98735a7c801be3e"></img>
+            </div>
+            <div>
+              <p>Liked Songs</p>
+              <div>
+                <span>핀</span>
+                <span>Playlist・631 songs</span>
+              </div>
+            </div>
+          </div>
+          <div style={{ display: 'flex', columnGap: '12px', alignItems: 'center', padding: '8px' }}>
+            <div style={{ width: '55px' }}>
+              <img style={{ borderRadius: '6px' }} src="https://i.scdn.co/image/ab67616d00001e024ba287ffc98735a7c801be3e"></img>
+            </div>
+            <div>
+              <p>Liked Songs</p>
+              <div>
+                <span>핀</span>
+                <span>Playlist・631 songs</span>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
-
     </div>
   );
 }
